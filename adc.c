42f2492741e040a1465f790060abbc84dc9fd52a
accden <pi@raspberrypi.(none)>
2013-08-01 10:44:07 +0000

    commit 2

--- adc.c
+++ adc.c
@@ -4,13 +4,13 @@
 #include <stdint.h>
 #include <unistd.h>
 
-#define sleep delayMicroseconds(3)
-
+//#define sleep delayMicroseconds(0.9)
+#define sleep mySleep()
 
 void mySleep()
 	{
 	int n=0;
-	for(int i=0;i<3000;i++)
+	for(int i=0;i<50;i++)
 		{
 		n+=1;
 		asm volatile("":::);
@@ -55,59 +55,60 @@ int doubleread(int clock, int cs, int adc0, int adc1) {	//Input variables refer 
 	pinMode(adc0, INPUT);
 	pinMode(adc1, INPUT);
 	digitalWrite(cs, 1); //Initializes chip select to 1
-	digitalWrite(clock, 1); //Initializes clock to 1
+	digitalWrite(clock, 0); //Initializes clock to 1
 	sleep;
-	int flag=0,counter=0, xthresh=0, xgot=0, ythresh=0, ygot=0;
+	int arr[100],flag=0,counter=0, xthresh=0, xgot=0, ythresh=0, ygot=0;
 	volatile uint16_t x=0,y=0,dx=0,dy=0;
 	while(1)
+	//while(counter<100)
 		{
 		x=y=dx=dy=0;
 		digitalWrite(cs, 0); //Falling chip select initiates conversion and data transfer
-		sleep;
 		for(int i=0;i<2;i++)
 			{
-			digitalWrite(clock, 0);
-			sleep;
 			digitalWrite(clock, 1);
+			sleep;
+			digitalWrite(clock, 0);
 			sleep;
 			}
 		for(int j=0;j<12;j++) //Begin reading data at the falling edge after the null bit
 			{
-			digitalWrite(clock, 0);
+			digitalWrite(clock, 1);
+			sleep;
+		digitalWrite(clock, 0);
 			sleep;
 			dx = digitalRead(adc0);
 			dy = digitalRead(adc1);
-			//int dy = 1;
 			x=x|(dx<<(11-j));
 			y=y|(dy<<(11-j));
-			digitalWrite(clock, 1);
-			sleep;
 			}
-		//if(x>2200||y>2200)
-		//	flag=1;
-		//if((x>2200||x<1000)&&xgot==0)
-		//	{
-		//	xthresh=counter;
-		//	xgot=1;
-		//	}
-		//if((y>2200|y<1000)&&ygot==0)
-		//	{
-		//	ythresh=counter;
-		//	ygot=1;
-		//	}
-		//if(x>2300||y>2300)
-		//	printf("%d\t%d\t%d\n",xthresh,ythresh,counter);
-		printf("%d\t%d\n",x,y);
+		if((x>2300&&x<2999)&&xgot==0)
+			{
+			xthresh=counter;
+			printf("%d\n",xthresh);
+			xgot=1;
+			}
+		if((y>2300&&y<2999)&&ygot==0)
+			{
+			ythresh=counter;
+			printf("%d\n",ythresh);
+			ygot=1;
+			}
+		//arr[counter]=x;
+		//if((x>2300&&x<2999)||y>2300)
+		//	printf("%d\t%d\n",x,y);
+		//	printf("%d\n",counter);
 		digitalWrite(cs, 1); //Raise chip select
-		sleep;
 		sleep;
 		counter++;
 		}	
-	//printf("%d\t%d\n",x,y);
+	for(int i=0;i<100;i++)
+		printf("%d\n",arr[i]);
 	}
 
 
 int main() {
+	piHiPri(99);
 	if (wiringPiSetup () == -1)
 		exit (1);
 	//singleread(7,14,11);
