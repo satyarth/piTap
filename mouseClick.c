3b1145c3279e34904996fd80af397f9dd469ae45
accden <satyarth.mishra@gmail.com>
2013-08-16 02:07:41 +0100

    bug

--- mouseClick.c
+++ mouseClick.c
@@ -1,11 +1,9 @@
 #include <X11/Xlib.h>
-#include<stdio.h>
-#include<unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <X11/Xlib.h>
-#include <X11/Xutil.h>
+#include <X11/Xresource.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "mouseClick.h"
 
 void mouseClick(int button)
