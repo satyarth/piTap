836cda1a379a050ae48dc4ca149706eb7486d930
accden <satyarth.mishra@gmail.com>
2013-08-16 11:47:57 +0100

    test

--- test.c
+++ test.c
@@ -1,54 +1,20 @@
-#include <X11/Xlib.h>
-#include <X11/Xresource.h>
-#include <X11/Xutil.h>
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "mouseClick.h"
-
-void mouseClick(int button)
-	{
-    Display *display = XOpenDisplay(NULL);
-    XEvent event;
-    if(display == NULL)
-		{
-        fprintf(stderr, "Error\n");
-        exit(EXIT_FAILURE);
-		}
-    memset(&event, 0x00, sizeof(event));
-    event.type = ButtonPress;
-    event.xbutton.button = button;
-    event.xbutton.same_screen = True;
-    XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-    event.xbutton.subwindow = event.xbutton.window;
-    while(event.xbutton.subwindow)
-		{
-        event.xbutton.window = event.xbutton.subwindow;
-        XQueryPointer(display, event.xbutton.window, &event.xbutton.root, &event.xbutton.subwindow, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-		}
-    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0)
-		fprintf(stderr, "Error\n");
-    XFlush(display);
-    usleep(100000);
-    event.type = ButtonRelease;
-    event.xbutton.state = 0x100;
-    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0)
-		fprintf(stderr, "Error\n");
-    XFlush(display);
-    XCloseDisplay(display);
+#include<graphics.h>
+#include<conio.h>
+ 
+int main()
+{
+   int gd = DETECT,gm,left=100,top=100,right=200,bottom=200,x= 300,y=150,radius=50;
+ 
+   initgraph(&gd, &gm, "C:\\TC\\BGI");
+ 
+   rectangle(left, top, right, bottom);
+   circle(x, y, radius);
+   bar(left + 300, top, right + 300, bottom);
+   line(left - 10, top + 150, left + 410, top + 150);
+   ellipse(x, y + 200, 0, 360, 100, 50);
+   outtextxy(left + 100, top + 325, "My First C Graphics Program");
+ 
+   getch();
+   closegraph();
+   return 0;
 }
-void clickCoords(int x,int y)
-	{
-    Display *display = XOpenDisplay(0);
-	Window root = DefaultRootWindow(display);
-    XWarpPointer(display, None, root, 0, 0, 0, 0, x, y);
-	mouseClick(Button1);
-	XFlush(display);
-    XCloseDisplay(display);
-    }
-	
-void main()
-	{
-	clickCoords(45,45);
-	}
