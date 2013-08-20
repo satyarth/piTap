1a10cce11149b868b26e20f3bd4a9ecdd67c1ccc
accden <satyarth.mishra@gmail.com>
2013-08-20 11:15:21 +0100

    Reading velocity from file again

--- fmin.c
+++ fmin.c
@@ -25,7 +25,7 @@ float error(float x,float y,float x1,float y1,float x2,float y2,float tdiff)
 	return(error);
 	}
 	
-float cost_function(float x,float y,float d_ba,float d_dc,float d_ca,float d_db)
+float cost_function(float x,float y,float d_ba,float d_dc,float d_ca,float d_db,float v)
 	{
 	float cost;
 	cost=error(x,y,x_b,y_b,x_a,y_a,d_ba)*error(x,y,x_b,y_b,x_a,y_a,d_ba)+error(x,y,x_d,y_d,x_c,y_c,d_dc)*error(x,y,x_d,y_d,x_c,y_c,d_dc)+error(x,y,x_c,y_c,x_a,y_a,d_ca)*error(x,y,x_c,y_c,x_a,y_a,d_ca)+error(x,y,x_d,y_d,x_b,y_b,d_db)*error(x,y,x_d,y_d,x_b,y_b,d_db);
@@ -38,16 +38,21 @@ int * findPosition(int a, int b, int c, int d) //Reads timing differences from a
 	static int coords[2];
 	float minx=50,miny=4;	
 	float d_ba,d_dc,d_ca,d_db,min;
+	const float v;
+	FILE *ifp;
+	ifp = fopen("calibration","r");
+	fscanf(ifp, "%d", v);
+	fclose(ifp); 
 	d_ba=v*((float)a-(float)b);
 	d_dc=v*((float)c-(float)d);
 	d_ca=v*((float)a-(float)c);
 	d_db=v*((float)b-(float)d);
 	float val[x_d][y_d];
-	min=cost_function(50,50,d_ba,d_dc,d_ca,d_db);
+	min=cost_function(50,50,d_ba,d_dc,d_ca,d_db,v);
 	for(i=0;i<divisions_x;i++)
 		for(j=0;j<divisions_y;j++)
 			{
-			val[i][j]=cost_function(i*(x_d-x_a)/divisions_x,j*(y_d-y_a)/divisions_y,d_ba,d_dc,d_ca,d_db);
+			val[i][j]=cost_function(i*(x_d-x_a)/divisions_x,j*(y_d-y_a)/divisions_y,d_ba,d_dc,d_ca,d_db,v);
 			if(val[i][j]<min)
 				{
 				min=val[i][j];
